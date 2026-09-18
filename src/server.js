feabe0a7c6247f29dd85db9b5078622f42144e82
mwanzia-simon <mwanziasimon160@gmail.com>
2026-09-18 17:19:57 +0300

    feat: create basic Discord bot with hello command

---
+++ src/server.js
@@ -0,0 +1,24 @@
+import "dotenv/config";
+import { Client, GatewayIntentBits } from "discord.js";
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.once("clientReady", (client) => {
+  console.log(`✅ Logged in as ${client.user.tag}`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === "!hello") {
+    message.reply("Hello! 👋");
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
