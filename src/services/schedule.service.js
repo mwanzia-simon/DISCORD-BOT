e4852af59cbefebc028545090b287e54b7623b0a
mwanzia-simon <mwanziasimon160@gmail.com>
2026-09-22 00:03:36 +0300

    feat: implement addClass function to create user class schedules

---
+++ src/services/schedule.service.js
@@ -0,0 +1,28 @@
+import Schedule from "../models/Schedule.js";
+
+export async function addClass(
+  course,
+  day,
+  startTime,
+  endTime,
+  location,
+  userID,
+) {
+  const lastClass = await Schedule.findOne({ user: userID }).sort({
+    classNumber: -1,
+  });
+
+  const classNumber = lastClass ? lastClass.classNumber + 1 : 1;
+
+  const schedule = await Schedule.create({
+    user: userID,
+    classNumber,
+    course,
+    day,
+    startTime,
+    endTime,
+    location,
+  });
+
+  return schedule;
+}
